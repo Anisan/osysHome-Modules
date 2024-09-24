5af3cb0fee0c62a23c76d034888b311e74981e1a
Eraser <eraser1981@gmail.com>
2024-09-24 09:34:30 +0300

    set log level for plugin

--- forms/ModuleForm.py
+++ forms/ModuleForm.py
@@ -1,11 +1,10 @@
+import json
 from flask import render_template, redirect
-from flask_wtf import  FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired
-
 from app.extensions import cache
 from app.database import db
-
 from app.core.models.Plugins import Plugin
 
 class ModuleForm(FlaskForm):
@@ -14,17 +13,33 @@ class ModuleForm(FlaskForm):
     hidden = BooleanField('Hidden in statusbar')
     active = BooleanField('Active')
     url = StringField('Url repository Github')
+    level_logging = SelectField("Level logging (apply after restart)",
+                                choices=(
+                                    (None,"Default"),
+                                    ("DEBUG","Debug"),
+                                    ("INFO","Info"),
+                                    ("WARNING","Warning"),
+                                    ("ERROR","Error"),
+                                    ("CRITICAL","Critical")
+                                ),
+                                validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 def routeSettings(request):
     name = request.args.get('name',None)
     module = Plugin.query.filter(Plugin.name == name).one_or_404()
     form = ModuleForm(obj=module)
+    config = {}
+    if module.config:
+        config = json.loads(module.config)
 
     if form.validate_on_submit():
         form.populate_obj(module)
+        config['level_logging'] = form.level_logging.data
+        module.config = json.dumps(config)
         db.session.commit()
         cache.delete('sidebar')
         return redirect("Modules")
-    
+
+    form.level_logging.data = config.get('level_logging', None)
     return render_template("module.html", name=name, form=form)
